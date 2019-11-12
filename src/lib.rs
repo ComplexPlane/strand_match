ba18a995a2bbc3637bf8e265624276e7baedd49a
ComplexPlane <complexplanegames@gmail.com>
2019-11-12 17:20:36 -0500

    Move scripts to complexplane/ghidra_scripts
    Also, export section headers for Dolphin mapfiles

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ type Result<T> = std::result::Result<T, failure::Error>;
 
 pub fn run() -> Result<()> {
     let sdk_funcs = parse::parse_dir("ghidra-export/sdk")?;
-    let rel_funcs = parse::parse_dir("ghidra-export/smb1")?;
+    let rel_funcs = parse::parse_dir("ghidra-export/smb2")?;
 
     matching::match_funcs(sdk_funcs, rel_funcs)?;
 
--- src/mapfile.rs
+++ src/mapfile.rs
@@ -5,11 +5,11 @@ use crate::function::AsmFunction;
 use crate::Result;
 
 // Writes a Dolphin-compatible symbol map which can be imported into Ghidra with the script
-//
 
 pub fn export_mapfile(pairings: &[(&AsmFunction, &AsmFunction)]) -> Result<()> {
     let mut mapfile = File::create("smb2-symbols.map")?;
 
+    writeln!(mapfile, ".text section layout")?;
     for (sdk, rel) in pairings {
         writeln!(mapfile, "{:08x} {:08x} {:08x} 0 {}",
                  rel.ghidra_addr,
@@ -17,5 +17,7 @@ pub fn export_mapfile(pairings: &[(&AsmFunction, &AsmFunction)]) -> Result<()> {
                  rel.ghidra_addr,
                  sdk.full_name())?;
     }
+    writeln!(mapfile, "\n.data section layout")?;
+
     Ok(())
 }
